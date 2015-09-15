3dab0c1f5a7be15f66659265a7a8b32de3376fdf
John Finigan <johnwfinigan@gmail.com>
2015-09-15 17:11:09 -0500

    enhanced with more lseek examples and much better comments

--- seek-examples/lseek-example1.c
+++ seek-examples/lseek-example1.c
@@ -1,6 +1,7 @@
-/* modified copy program to always skip first 100
- * bytes of source file. so, not a copy program 
- * anymore. */
+/* Demonstrates some uses of lseek.
+ * modified copy program to always skip first 100
+ * bytes of source file. modified to add extra
+ * data to the end of the destination file.  */
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -33,6 +34,8 @@ int main(int argc, char **argv)
     ssize_t rret = 0;
     char buf[1024];
 
+    /* seek past first 100 bytes of the source file.
+     * they are skipped in the copy. */
     off_t pos = lseek(sourcefile, 100, SEEK_SET);
     if (pos == -1) {
 	perror("lseek");
@@ -46,13 +49,16 @@ int main(int argc, char **argv)
 	    perror("read error");
 	    exit(3);
 	}
-	off_t currpos = lseek(sourcefile, 0, SEEK_CUR);
-	if (currpos == -1) {
+
+	/* seeking 0 bytes away from the current position
+	 * just causes lseek to return the current position */
+	pos = lseek(sourcefile, 0, SEEK_CUR);
+	if (pos == -1) {
 	    perror("lseek");
-	    exit(8);
+	    exit(9);
 	}
 	printf("current position in source file during loop: %lld\n",
-	       (long long) currpos);
+	       (long long) pos);
 
 	ssize_t wret = 0;
 	ssize_t wcount = 0;
@@ -65,6 +71,33 @@ int main(int argc, char **argv)
 	    wcount += wret;
 	} while (wcount < rret);
     }
+
+    /* move the current postion in the dest file to the end of the 
+     * destfile */
+    pos = lseek(destfile, 0, SEEK_END);
+    if (pos == -1) {
+	perror("lseek");
+	exit(10);
+    }
+    printf("current position in dest file (end of dest file): %lld\n",
+	   (long long) pos);
+
+    /* write some extra data to end of dest file. manually calcuated length.
+     * not partial-write safe. */
+    int ret = write(destfile, "Some extra data\n", 16);
+    if (ret == -1) {
+	perror("error writing extra data to dest");
+	exit(11);
+    }
+    /* get position in dest file after write */
+    pos = lseek(destfile, 0, SEEK_CUR);
+    if (pos == -1) {
+	perror("lseek");
+	exit(9);
+    }
+    printf
+	("current position in dest file after writing extra data: %lld\n",
+	 (long long) pos);
 
     /* explicit close isn't useful if you dont check its return... */
     int scret = close(sourcefile);

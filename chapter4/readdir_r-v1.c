54b18f3e797557baeca0eb6fce51e2d19a895aa9
John Finigan <johnwfinigan@gmail.com>
2015-10-21 11:43:10 -0500

    better comments

--- chapter4/readdir_r-v1.c
+++ chapter4/readdir_r-v1.c
@@ -8,13 +8,15 @@
 int main(int argc, char **argv)
 {
 
+    /* if argc < 2, no optional command line args: default to the current directory */
     char *d_to_open = ".";
 
     if (argc == 2) {
+	/* argv[1] has type char *, and so does d_to_open */
 	d_to_open = argv[1];
     }
     if (argc > 2) {
-	printf("please specify directory name to open\n");
+	printf("please specify one directory name to open\n");
 	exit(1);
     }
 
@@ -24,18 +26,30 @@ int main(int argc, char **argv)
 	exit(2);
     }
 
-    struct dirent d;
-    struct dirent *res;
+    struct dirent d;   /* manpage states that readdir_r's entry
+                          buffer must be "caller-allocated". That's you!
+                          In other words, you have to declare an object
+                          that is big enough to hold a dirent struct's 
+                          worth of data. Just declaring the struct is
+                          an easy way to do it. */
+    struct dirent *result;
 
 
     while (1) {
 
-	int ret = readdir_r(dstream, &d, &res);
-	if (ret > 0) {
+	int ret = readdir_r(dstream, &d, &result);
+        /* note that unlike readdir, readdir_r returns 0 for 
+        *  success and non-zero for error. The error and end
+        *  of directory states do NOT have the same return value */
+
+	if (ret != 0) {
 	    perror("readdir_r error");
 	    exit(3);
 	}
-	if (!res) {
+	if (!result) { /* readdir_r stores NULL in result upon end of 
+                        * directory. NULL is false, so !NULL is true.
+                        * Personally I prefer to write (result == NULL),
+                        * but this is common and it's up to you. */
 	    break;
 	}
 

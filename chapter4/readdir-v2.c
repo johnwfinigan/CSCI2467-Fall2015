23716c41faf72ea28ed28c5aeda44cd07d277c27
John Finigan <johnwfinigan@gmail.com>
2015-10-21 11:24:29 -0500

    better comments

--- chapter4/readdir-v2.c
+++ chapter4/readdir-v2.c
@@ -7,14 +7,15 @@
 
 int main(int argc, char **argv)
 {
-
+    /* if argc < 2, no optional command line args: default to the current directory */
     char *d_to_open = ".";
 
     if (argc == 2) {
+	/* argv[1] has type char *, and so does d_to_open */  
 	d_to_open = argv[1];
     }
     if (argc > 2) {
-	printf("please specify directory name to open\n");
+	printf("please specify one directory name to open\n");
 	exit(1);
     }
 
@@ -27,18 +28,29 @@ int main(int argc, char **argv)
     struct dirent *d;
 
     /* remember! readdir returns NULL when at end of 
-     *      directory OR when an error occurs */
+     *      directory OR when an error occurs. The only way
+     *      to tell the difference is to see if errno has been 
+     *      changed after readdir returns NULL. Fortunately,
+     *      we are allowed to set errno to a known value.
+     *
+     *      Must set errno directly before calling readdir,
+     *      and check directly after, because errno is global.
+     *      Any failing system call in your code could change
+     *      errno between the set and the check, unless you 
+     *      put nothing but readdir between the set and the check. */
 
     while (1) {
 
-	errno = 0;
+	errno = 0; 
 	d = readdir(dstream);
 	if (d == NULL) {
 	    if (errno != 0) {
 		perror("readdir error");
 		exit(3);
 	    }
-	    break;
+	    else {
+	    	break; /* errno was not changed. end of directory */
+            }
 	}
 
 	printf("inode: %lld, name: %s\n", (long long) d->d_ino, d->d_name);
